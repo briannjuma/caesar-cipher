c08e7e6b406443c8033499d76838824afb934fe6
jumaBrian <njengabrianj@gmail.com>
2022-04-28 23:04:27 +0300

    Run successful test for Decryption

--- src/test/java/behaviourTests/DecoderTest.java
+++ src/test/java/behaviourTests/DecoderTest.java
@@ -1,4 +1,40 @@
 package behaviourTests;
 
+import brayocodes.Decoder;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DecoderTest {
+    Decoder testDecoder = new Decoder("BOY",2);
+
+    @Test
+    public void testInstanceCreated(){
+        assertEquals(true, testDecoder instanceof Decoder);
+    }
+
+    @Test
+    public void testInputSaved(){
+        assertEquals("BOY", testDecoder.getUserInput());
+    }
+    @Test
+    public void testInputNotEmpty(){
+        assertEquals(true, testDecoder.isNotEmpty());
+    }
+
+    @Test
+    public void testKeySaved(){
+        assertEquals(2, testDecoder.getShiftKey());
+    }
+    @Test
+    public void testKeyValid(){
+        assertEquals(true, testDecoder.isKeyValid());
+    }
+
+    @Test
+    public void testDecrypt(){
+        Decoder testDecrypt = new Decoder("DQA",2);
+        assertEquals("BOE",testDecoder.decrypt());
+    }
+
 }

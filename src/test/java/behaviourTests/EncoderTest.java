65a7b6c7010841387f8a0d6eee776c5c4e16719d
jumaBrian <njengabrianj@gmail.com>
2022-04-28 22:23:07 +0300

    Run successful test coverage for Encrption

--- src/test/java/behaviourTests/EncoderTest.java
+++ src/test/java/behaviourTests/EncoderTest.java
@@ -1,4 +1,44 @@
 package behaviourTests;
 
+import brayocodes.Encoder;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class EncoderTest {
+    Encoder testEncoder = new Encoder("BOY",3); // import our class
+
+    // Test 1
+    @Test
+    public void testNewInstanceCreated(){
+        assertEquals(true, testEncoder instanceof Encoder);
+    }
+
+    // Test 2 - user input
+    @Test
+    public void testInputSaved(){
+        assertEquals("BOY", testEncoder.getUserInput());
+    }
+    @Test
+    public void testInputNotEmpty(){
+        assertEquals(true, testEncoder.isNotEmpty());
+    }
+
+    // Test 3 - shift key
+    @Test
+    public void testKeySaved(){
+        assertEquals(3, testEncoder.getShiftKey());
+    }
+    @Test
+    public  void testKeyValid(){
+        assertEquals(true, testEncoder.isKeyValid());
+    }
+
+    //Test 4
+    @Test
+    public void testEncrypt(){
+        Encoder testEncrypt = new Encoder("BOY",2);
+        assertEquals("DQA", testEncrypt.encrypt());
+    }
 }
